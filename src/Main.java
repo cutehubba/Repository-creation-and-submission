b81a4697573bbd3fd55a79ba77a5d2c3c1dfe206
2022111770 <2609834220@qq.com>
2024-06-05 05:03:28 +0800

    push

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        //add a line of comment
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
